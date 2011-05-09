7178916a06b72d38c52c6852c1900b2b00209941
andr.bun@gmail.com <andr.bun@gmail.com@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2011-05-09 11:47:36 +0000

    initialization

---
+++ Cropland Tools/Probability Layer/config.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 11:36:34 2010
+
+@author: Andriy Bun
+
+@name:   Useful utils
+"""
+
+import os
+import shutil
+import arcgisscripting
+from iterableStruct import iterableStruct
+
+class config():
+    
+    num_rasters = 4
+    
+    paths = iterableStruct()
+
+    def __init__(self, gui, inputPaths, coords, doClip = 1):
+        self.gui = gui
+        self.gp = arcgisscripting.create()
+        self.gp.CheckOutExtension("Spatial")
+        self.gp.OverWriteOutput = 1
+        self.__InitPaths__(inputPaths)
+        self.num_rasters = len(self.paths.inputs.LayerList)
+        gui.PrintTextTime("Verifying")
+        self.VerifyRasters(self.paths.inputs, gui)
+        gui.PrintTextTime("Clipping inputs")
+        if doClip == 1:
+            self.ClipRasters(coords)
+
+    def __InitPaths__(self, inputPaths = None):
+        # Main directories
+        self.paths.HOMEDIR = os.getcwd() + "\\"
+#        self.paths.INPUTDIR = self.paths.HOMEDIR + "input_5\\"
+        self.paths.INPUTDIR = self.paths.HOMEDIR + "input\\"
+        self.paths.RESULTDIR = self.paths.HOMEDIR + "results\\"
+        self.paths.TMPDIR = self.paths.HOMEDIR + "tmp\\"
+        
+        self.DeleteDir(self.paths.TMPDIR)
+        if not os.path.isdir(self.paths.TMPDIR):
+            os.mkdir(self.paths.TMPDIR)
+        
+        if inputPaths is None:
+            # Input rasters:
+            self.paths.inputs = iterableStruct()
+            
+            inputLayerPrefix = self.paths.INPUTDIR + "layer"
+            self.paths.inputs.units            = self.paths.INPUTDIR + "countries"
+            self.paths.inputs.cellAreas        = self.paths.INPUTDIR + "cell_areas"
+            
+            self.paths.inputs.LayerList = []
+            
+            for i in range(1, self.num_rasters + 1):
+                self.paths.inputs.LayerList.append(inputLayerPrefix + str(i))
+    
+            # Result:
+            if not os.path.isdir(self.paths.RESULTDIR):
+                os.mkdir(self.paths.RESULTDIR)
+
+            self.paths.result = self.paths.RESULTDIR + "mark_high"
+            self.paths.resultAvg = self.paths.RESULTDIR + "mark_high_a"
+        else:
+            # Input rasters:
+            self.paths.inputs = iterableStruct()
+            
+            self.paths.inputs.units     = inputPaths.countries
+            self.paths.inputs.cellAreas = inputPaths.cellAreas
+            self.paths.inputs.LayerList = inputPaths.croplandLayerList
+            
+            # Result:
+            self.paths.result = inputPaths.result
+            self.paths.resultAvg = inputPaths.resultAvg
+        
+        # Temporary rasters:
+        self.paths.tmp = iterableStruct()
+
+        tmpPrefix = self.paths.TMPDIR + "tmp"
+        
+        self.paths.tmp.LayerList = []
+        
+        for i in range(1, len(self.paths.inputs.LayerList) + 1):
+            self.paths.tmp.LayerList.append(tmpPrefix + str(i))
+
+        self.paths.tmp.temp1      = self.paths.TMPDIR + "temp1"
+        self.paths.tmp.temp2      = self.paths.TMPDIR + "temp2"
+        self.paths.tmp.temp3      = self.paths.TMPDIR + "temp3"
+        self.paths.tmp.temp4      = self.paths.TMPDIR + "temp4"
+        self.paths.tmp.sumRast    = self.paths.TMPDIR + "sumRast"
+        self.paths.tmp.sumRastOne = self.paths.TMPDIR + "sumRastOne"
+        self.paths.tmp.sumRastTwo = self.paths.TMPDIR + "sumRastTwo"
+
+    # clip an area from input raster    
+    def ClipRasters(self, coords):
+        if coords is None or coords == '#':
+            RasterName = self.paths.inputs.units
+            rName = os.path.basename(RasterName)
+            clippedName = self.paths.TMPDIR + rName
+            self.gp.copy_management(RasterName, clippedName)
+            self.paths.inputs.units = clippedName
+            RasterName = self.paths.inputs.cellAreas
+            rName = os.path.basename(RasterName)
+            clippedName = self.paths.TMPDIR + rName
+            self.gp.copy_management(RasterName, clippedName)
+            self.paths.inputs.cellAreas = clippedName
+        else:
+            RasterName = self.paths.inputs.units
+            rName = os.path.basename(RasterName)
+            clippedName = self.paths.TMPDIR + rName
+            self.gp.clip_management(RasterName, coords, clippedName)
+            self.paths.inputs.units = clippedName
+            RasterName = self.paths.inputs.cellAreas
+            rName = os.path.basename(RasterName)
+            clippedName = self.paths.TMPDIR + rName
+            self.gp.clip_management(RasterName, coords, clippedName)
+            self.paths.inputs.cellAreas = clippedName
+        clippedLayerList = []
+        
+        for RasterName in self.paths.inputs.LayerList:
+            rName = os.path.basename(RasterName)
+            clippedName = self.paths.TMPDIR + rName
+            if coords is None or coords == '#':
+                self.gp.copy_management(RasterName, clippedName)
+            else:
+                self.gp.clip_management(RasterName, coords, clippedName)
+            clippedLayerList.append(clippedName)
+        self.paths.inputs.LayerList = clippedLayerList
+                
+    # Verify if rasters exist
+    def VerifyRasters(self, ListOfRasters, gui):
+        flag = True
+        for RasterName in ListOfRasters:
+            if type(RasterName) == list:
+                self.VerifyRasters(RasterName, gui)
+            else:
+                if (not self.gp.Exists(RasterName)):
+                    gui.Error('Raster does not exist: ' + RasterName)
+                    flag = False
+        return flag
+
+    # Delete all rasters in a list
+    def DeleteRasters(self, ListOfRasters):
+        for RasterName in ListOfRasters:
+            if type(RasterName) == list:
+                self.DeleteRasters(RasterName)
+            else:
+                self.gp.delete_management(RasterName)
+
+    # Delete all files from a directory
+    def DeleteDir(self, dir):
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
---
+++ Cropland Tools/Probability Layer/utils.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 15:36:20 2010
+
+@author: Andriy Bun
+
+@name:   Useful functions
+"""
+
+import math
+
+#===============================================================================
+#  Method to replace all values in a raster with 'weight', and zeros or noData
+#  with 0
+#===============================================================================
+def MakeRasterOfValues( gp, rast, order, weight, weight2, out ):
+    gp.Con_sa(rast, order, out, "0", "VALUE > 0")
+    gp.BuildRasterAttributeTable_management(out,"OVERWRITE")
+    gp.Con_sa(rast, weight, out + "one", "0", "VALUE > 0")
+    gp.BuildRasterAttributeTable_management(out + "one","OVERWRITE")
+    gp.Con_sa(rast, weight2, out + "two", "0", "VALUE > 0")
+    gp.BuildRasterAttributeTable_management(out + "two","OVERWRITE")
+
+#===============================================================================
+# Validation function
+#===============================================================================
+def Validate(interface, runConfig, priorityValues = None, priorityValues2 = None):
+    interface.PrintText('Validating result...')
+    import arcgisscripting
+    from rasterAgreementTable import rasterAgreementTable
+    inputs = runConfig.paths.inputs
+    result = runConfig.paths.result
+    gp = arcgisscripting.create()
+    gp.CheckOutExtension("Spatial")
+    gp.OverWriteOutput = 1
+    
+    inputList = inputs.LayerList
+    
+    if priorityValues is None:
+        priorityValues = []
+        for i in range(0, runConfig.num_rasters):
+            priorityValues.append(1)
+    
+    if priorityValues2 is None:
+        priorityValues2 = []
+        for i in range(0, runConfig.num_rasters):
+            priorityValues2.append(1)
+    
+    agreementTable = rasterAgreementTable(priorityValues, priorityValues2)
+    
+#    weights = GetWeights(priorityValues)
+
+    # Land classes:
+#    landClasses = GetClasses(weights)
+
+#    interface.PrintText(str(weights))
+#    interface.PrintText(str(landClasses))
+
+    xMin = gp.GetRasterProperties(result, 'LEFT')
+    yMin = gp.GetRasterProperties(result, 'BOTTOM')
+    xMax = gp.GetRasterProperties(result, 'RIGHT')
+    yMax = gp.GetRasterProperties(result, 'TOP')
+    
+    print xMin
+    print xMax
+    print yMin
+    print yMax
+    
+    xCount = gp.GetRasterProperties(result, 'COLUMNCOUNT')
+    yCount = gp.GetRasterProperties(result, 'ROWCOUNT')
+    
+    xCellSize = gp.GetRasterProperties(result, 'CELLSIZEX')
+    yCellSize = gp.GetRasterProperties(result, 'CELLSIZEY')
+    
+    xRes = 100
+    yRes = 100
+    
+#    print xRes
+#    print yRes
+    
+    xStep = xCellSize * math.floor(xCount / xRes)
+    yStep = yCellSize * math.floor(yCount / yRes)
+    
+    print xStep
+    print yStep
+    
+    xGrid = []
+    yGrid = []
+    
+    for i in xrange(xRes):
+        xGrid.append(xMin + xCellSize / 2 + xStep * (i+1))
+
+    for i in xrange(yRes):
+        yGrid.append(yMin + yCellSize / 2 + yStep * (i+1))
+
+    numPoints = 0
+
+    for x in xGrid:
+        for y in yGrid:
+            coord = str(x) + ' ' + str(y)
+            if (gp.GetCellValue_management(result, coord) == 'NoData') or (float(gp.GetCellValue_management(result, coord)) == 0):
+                break
+            inputValues = []
+            rastersAgree = 0
+            idx = 0
+            for raster in inputList:
+                val = gp.GetCellValue_management(raster, coord)
+                inputValues.append(val)
+                if not (float(val) == 0):
+                    rastersAgree = rastersAgree + agreementTable.weights[idx]
+                idx = idx + 1
+            resultValue = gp.GetCellValue_management(result, coord)
+            if resultValue =='NoData':
+                resultValue = 0
+#            interface.PrintText(str(resultValue))
+#            interface.PrintText(str(inputValues))
+#            interface.PrintText(str(agreementTable.data[int(resultValue)]['classes1']))
+#            interface.PrintText(str(rastersAgree))
+#            interface.PrintText(str(gp.GetCellValue_management(inputs.units, coord)))
+#            interface.PrintText('=====')
+            if not (rastersAgree == agreementTable.data[int(resultValue)]['classes1']):
+                interface.PrintText('Point: (' + str(x) + ', ' + str(y) + '): Expected rasters agree - ' + str(rastersAgree) + ', Computed - ' + str(agreementTable.data[int(resultValue)]['classes1']))
+                interface.Error('Result validation failed')
+            numPoints = numPoints + 1
+    interface.PrintText('Result validation completed successfully on ' + str(numPoints) + ' point(s)')
+
+#===============================================================================
+# Find a value in a sorted list
+#===============================================================================
+def findFirst(lst, val):
+    lo = 0
+    hi = len(lst)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        midval = lst[mid]
+        if midval < val:
+            lo = mid+1
+        elif midval > val: 
+            hi = mid
+        else:
+            while mid > 0 and lst[mid-1] == val:
+                mid = mid - 1
+            return mid
+    return -1
+"""
+#===============================================================================
+# Method to parse rasters' priorities to weights
+#===============================================================================
+def GetWeights(priorityValues):
+    priorityValuesSorted = priorityValues[:]
+    priorityValuesSorted.sort()
+    numValues = len(priorityValues)
+    priorityCumSum = [1]
+    weights = []
+    idx = 0
+    for i in range(1, numValues):
+        priorityCumSum.append(priorityCumSum[i-1] + priorityCumSum[idx])
+        if not(priorityValuesSorted[i] == priorityValuesSorted[i-1]):
+            idx = i
+    # Put weights in proper order
+    for val in priorityValues:
+        weights.append(max(1, priorityCumSum[findFirst(priorityValuesSorted, val)]))
+
+#    # Debug
+#    print priorityValues
+#    print priorityValuesSorted
+#    print priorityCumSum
+#    print weights
+    return weights
+
+
+#===============================================================================
+# Another handy function
+#===============================================================================
+global classes
+global agreementTable
+global curRow
+
+#===============================================================================
+# TODO: Process table
+#===============================================================================
+
+def GetWeightSumClasses(weights, weightsSum):
+    global classes
+    global agreementTable
+    global curRow
+    if len(weights) > 1:
+        agreementTable[curRow].append(1)
+        GetWeightSumClasses(weights[1:len(weights)], weightsSum + weights[0])
+        for i in agreementTable[curRow-1][0:-len(weights)]:
+            agreementTable[curRow].append(i)
+        agreementTable[curRow].append(0)
+        GetWeightSumClasses(weights[1:len(weights)], weightsSum)
+    else:
+        classes.append(weightsSum + weights[0])
+        classes.append(weightsSum)
+        for i in agreementTable[curRow]:
+            agreementTable[curRow+1].append(i)
+        agreementTable[curRow].append(1)
+        agreementTable[curRow+1].append(0)
+        curRow += 2
+    return classes
+
+def GetClasses(weights):
+    global classes
+    global agreementTable
+    global curRow
+    classes = []
+    agreementTable = []
+    curRow = 0
+    print '===='
+    for i in range(0, 2**len(weights)):
+        agreementTable.append([])
+    classes = GetWeightSumClasses(weights, 0)
+    tab = []
+    for i in range(0, 2**len(weights)):
+        tab.append(dict(classes = classes[i], agreementTable = agreementTable[i]))
+        #print agreementTable[i]
+        print tab[i]
+    classes.sort()
+    tab.sort()
+    for i in range(0, 2**len(weights)):
+        print tab[i]
+    return classes
+"""
