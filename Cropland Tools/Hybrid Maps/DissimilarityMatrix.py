456aace537b2b234afa5283ea2e3f1325188d10c
andr.bun <andr.bun@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2011-09-08 19:08:27 +0000

    fixed problems after bad merge

--- Cropland Tools/Hybrid Maps/DissimilarityMatrix.py
+++ Cropland Tools/Hybrid Maps/DissimilarityMatrix.py
@@ -9,5 +9,13 @@ def ReadDissimilarityMatrix(fileName):
             row.append(int(val))
         matrix.append(row)
     
+    # copy upper right matrix part to lower left to make it symmetric
+    matrixSize = len(matrix)
+    for idx1 in range(0, matrixSize):
+        if not(len(matrix[idx1]) == matrixSize):
+            raise Exception('Dissimilarity matrix dimensions must agree')
+        for idx2 in range(idx1+1, matrixSize):
+            matrix[idx2][idx1] = matrix[idx1][idx2]
+    
     return matrix
     
--- Cropland Tools/Hybrid Maps/main.py
+++ Cropland Tools/Hybrid Maps/main.py
@@ -7,7 +7,7 @@ Created on Fri May 13 13:39:39 2011
 
 import sys
 import os
-commonDir = os.path.dirname(sys.argv[0]) + '\\..\\Common'
+commonDir = os.path.dirname(sys.argv[0]) + '/../Common'
 sys.path.append(commonDir)
 
 from iterableStruct import iterableStruct
@@ -20,24 +20,25 @@ if __name__ == '__main__':
 #    gui = GeoprocessingInfo()
     gui = GeoprocessingInfo_debug()
     
-    #inputPaths.croplandLayerList = sys.argv[3].split(";")
-    #for i in range(len(inputPaths.croplandLayerList)):
-    #    inputPaths.croplandLayerList[i] = inputPaths.croplandLayerList[i][1:-1]
+    #paths.input.landMaps = sys.argv[3].split(";")
+    #for i in range(len(paths.input.landMaps)):
+    #    paths.input.landMaps[i] = paths.input.landMaps[i][1:-1]
     
     paths = iterableStruct()
+    
+#    paths.DATA_DIR = 'C:/ProgramFiles/Cropland Hybrid/in/'
+    paths.DATA_IN_DIR = 'D:/Workspace/IIASA/ArcGIS_Scripting_Hybrid_Maps/Cropland Hybrid/in/'
+    paths.DATA_OUT_DIR = 'D:/Workspace/IIASA/ArcGIS_Scripting_Hybrid_Maps/Cropland Hybrid/out/'
+    
     paths.input = iterableStruct()
+    paths.output = iterableStruct()
     paths.input.landMaps = []
-    paths.input.landMaps.append('C:\\ProgramFiles\\Cropland Hybrid\\in\\mod_eu1')
-    paths.input.landMaps.append('C:\\ProgramFiles\\Cropland Hybrid\\in\\glob_eu1')
-    paths.input.landMaps.append('C:\\ProgramFiles\\Cropland Hybrid\\in\\glc_eu')
+    paths.input.landMaps.append(paths.DATA_IN_DIR + 'mod_eu1')
+    paths.input.landMaps.append(paths.DATA_IN_DIR + 'glob_eu1')
+    paths.input.landMaps.append(paths.DATA_IN_DIR + 'glc_eu')
     
     paths.input.dissimilarityMatrixFile = 'dissimilarity_matrix.txt'
     
+    paths.output.hybridMap = paths.DATA_OUT_DIR + 'hybrid.img'
+    
     ProduceHybridMap(gui, paths = paths)
-    
-
-    
-    
-    
-    
-    
