fee5882a8dfc79300216dfd9f60865a689616899
andr.bun <andr.bun@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2011-10-31 19:15:29 +0000

    started working on validating cell size

--- trunk/Cropland Tools/Common/common.py
+++ trunk/Cropland Tools/Common/common.py
@@ -1,4 +1,5 @@
 import math
+import arcgisscripting
 
 def MakeListString(inList):
     listString = ""
@@ -12,3 +13,19 @@ def CompareEq(val1, val2, precision):
     
 def CompareNe(val1, val2, precision):
     return not(CompareEq(val1, val2, precision))
+    
+def CompareCellSize(rasterList):
+    EPSILON = 10e-3
+    gp = arcgisscripting.create()
+    xSize0 = gp.GetRasterProperties(rasterList[0], 'CELLSIZEX')
+    ySize0 = gp.GetRasterProperties(rasterList[0], 'CELLSIZEY')
+    for raster in rasterList[1:]:
+        xSize = gp.GetRasterProperties(raster, 'CELLSIZEX')
+        ySize = gp.GetRasterProperties(raster, 'CELLSIZEY')
+        if CompareNe(xSize0, xSize, EPSILON):
+            return False
+        if CompareNe(ySize0, ySize, EPSILON):
+            return False
+    return True
+    
+    
