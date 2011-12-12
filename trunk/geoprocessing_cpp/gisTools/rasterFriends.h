bbd15eabc015a5ba2766ab62fb2b37698bdba850
andr.bun <andr.bun@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2011-12-12 21:40:31 +0000

    refactoring code in progress

---
+++ trunk/geoprocessing_cpp/gisTools/rasterFriends.h
@@ -0,0 +1,49 @@
+#ifndef RASTER_FRIENDS_H_
+#define RASTER_FRIENDS_H_
+
+#include <vector>
+
+using namespace std;
+
+#include "assertInt.h"
+#include "errorCodes.h"
+
+class raster;
+class tableT;
+
+struct runParamsT
+{
+	string tmpDir;
+	string resultDir;
+};
+
+void multipleRasterArithmetics(float (*func)(const vector<float> & ), 
+							   const vector<raster *> & inRastersVector, 
+							   raster & outRaster);
+void multipleRasterArithmetics(void (*func)(const vector<float> &, const vector<float> &, const vector<float> &, vector<float> &),
+							   const vector<raster *> & inRastersVector,
+							   vector<raster *> & outRastersVector);
+//void multipleRasterArithmeticsAsTable(float (*func)(const vector<float> & , vector<float> & ), 
+//									  const vector<raster *> & inRastersVector, 
+//									  tableT & outTable);
+
+// Cropland algorithms:
+void validateCropland(raster & inCroplandRaster,
+					  raster & inZoneRaster,
+					  raster & inClassRaster,
+					  raster & outCroplandRaster,
+					  raster & errorRaster);
+void validateCropland(raster & inCroplandRaster,
+					  raster & inZoneRaster,
+					  raster & inClassRaster,
+					  raster & outCroplandRaster);
+void calibrateCropland(raster & inCroplandRaster,
+					   raster & inClassRaster,
+					   raster & statisticsLevelUp,
+					   raster & statisticsLevel,
+					   raster & resultLevelUp,
+					   raster & resultLevel,
+					   raster & outCalibratedRasterLevel,
+					   const runParamsT & params);
+
+#endif
