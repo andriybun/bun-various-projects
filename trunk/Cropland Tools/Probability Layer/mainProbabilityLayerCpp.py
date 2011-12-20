765d5e7cc4464e1c46f00b6b13064f3e771dba4a
andr.bun <andr.bun@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2011-12-20 22:20:12 +0000

    Finished python part for probability layer program

--- trunk/Cropland Tools/Probability Layer/mainProbabilityLayerCpp.py
+++ trunk/Cropland Tools/Probability Layer/mainProbabilityLayerCpp.py
@@ -4,6 +4,8 @@
 
 import sys
 import os
+import subprocess
+import shutil
 
 from rasterAgreementTable import rasterAgreementTable
 
@@ -12,6 +14,9 @@ def ones(size):
     for xIdx in range(size):
         xRes.append(1)
     return xRes
+    
+def vectorToStr(vec):
+    return str(vec).replace('[', '').replace(']', '').replace(',', '')
 
 if __name__ == "__main__":
     workingDir = os.path.dirname(sys.argv[0])
@@ -22,33 +27,38 @@ if __name__ == "__main__":
     # Parse command line arguments:
     num_args = len(sys.argv)
     
-    cellAreas = sys.argv[1]
-    countries = sys.argv[2]
+    cellAreas = sys.argv[1].replace("'","")
+    countries = sys.argv[2].replace("'","")
     
-    croplandLayerList = sys.argv[3].split(";")
+    croplandLayerList = (sys.argv[3].replace("'","")).split(";")
     numRasters = len(croplandLayerList)
+    passCroplandLayerList = ''
     for i in range(numRasters):
         if (croplandLayerList[i][0] == '\''):
             croplandLayerList[i] = croplandLayerList[i][1:-1]
+        passCroplandLayerList = passCroplandLayerList + '"%s" ' % (croplandLayerList[i])
+    passCroplandLayerList = passCroplandLayerList[0:-1]
 
     # Parse priorities
-    if sys.argv[4] == '#':
+    prior = sys.argv[4].replace("'","")
+    if prior == '#':
         priorityValues = ones(numRasters)
     else:
-        priorityList = sys.argv[4].split(";")
+        priorityList = prior.split(";")
         priorityValues = []
         for priorityStr in priorityList:
             priorityValues.append(int(priorityStr))
-    if sys.argv[5] == '#':
+    prior = sys.argv[5].replace("'","")
+    if prior == '#':
         priorityValues2 = ones(numRasters)
     else:
-        priorityList2 = sys.argv[5].split(";")
+        priorityList2 = prior.split(";")
         priorityValues2 = []
         for priorityStr in priorityList2:
             priorityValues2.append(int(priorityStr))
     
     # Parse results' names:
-    resultProb = sys.argv[6]
+    resultProb = sys.argv[6].replace("'","")
     resultNameTuple = os.path.splitext(resultProb)
     descriptionFileName = resultNameTuple[0] + ".txt"
     resultAvg = resultNameTuple[0] + "_avg" + resultNameTuple[1]
@@ -62,4 +72,30 @@ if __name__ == "__main__":
     agreementTable.PrintToFile(descriptionFile, croplandLayerList)
     descriptionFile.close()
     
+    resultDir        = os.path.dirname(resultProb)
+    tmpDir           = resultDir + "\\tmp_" + os.getenv('COMPUTERNAME')
+    deleteTmpDir = False
+    if not os.path.exists(tmpDir):
+        os.mkdir(tmpDir)
+        deleteTmpDir = True
     
+    executeCommand = '"%s" "%s" "%s" %d %s %s %s "%s" "%s" "%s" "%s"' % ( \
+        runFileName, \
+        cellAreas, \
+        countries, \
+        numRasters, \
+        passCroplandLayerList, \
+        vectorToStr(priorityValues), \
+        vectorToStr(priorityValues2), \
+        resultProb, \
+        resultAvg, \
+        resultMin, \
+        resultMax)
+
+    callResult = subprocess.call(executeCommand)
+
+    if deleteTmpDir:
+        shutil.rmtree(tmpDir)
+        
+    if not(callResult == 0):
+        raise Exception('Error! Function returned error code %d!' % str(callResult))
