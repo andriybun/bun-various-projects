6927b29050f52ee9b5f57bcf45a8bc60fdb98c06
andr.bun <andr.bun@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2012-03-01 12:28:20 +0000

    Updated toolbox

--- Cropland Tools/Common/CheckZonesForData.py
+++ Cropland Tools/Common/CheckZonesForData.py
@@ -11,15 +11,6 @@ if __name__ == "__main__":
     # Load DLL
     testDll = cdll.LoadLibrary("gisToolsInterface.dll")
 
-    # Identify the in / out parameters of the function from DLL we will use
-    testDll.checkZonesForData.restype = c_int
-    testDll.checkZonesForData.argtypes = [ \
-        c_int, \
-        (c_char_p * numRasters), \
-        c_char_p, \
-        c_char_p, \
-        c_int]
-
     # Parse input params
     zoneRasterPath = os.path.splitext(sys.argv[1].replace("'",""))[0]
 
@@ -30,6 +21,15 @@ if __name__ == "__main__":
         if (croplandLayerList[i][0] == '\''):
             croplandLayerList[i] = croplandLayerList[i][1:-1]
         passCroplandLayerList[i] = (os.path.splitext(croplandLayerList[i])[0])
+
+    # Identify the in / out parameters of the function from DLL we will use
+    testDll.checkZonesForData.restype = c_int
+    testDll.checkZonesForData.argtypes = [ \
+        c_int, \
+        (c_char_p * numRasters), \
+        c_char_p, \
+        c_char_p, \
+        c_int]
 
     selectionThreshold = int(sys.argv[3])
     
