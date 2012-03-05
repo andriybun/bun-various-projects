d71bd7a3b20e0a15bd80333137d61baf144c4d1b
andr.bun <andr.bun@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2012-03-05 20:02:40 +0000

    Added project for computing normalized probability layer

--- geoprocessing_cpp/gisToolsInterface/gisToolsInterface.cpp
+++ geoprocessing_cpp/gisToolsInterface/gisToolsInterface.cpp
@@ -15,18 +15,17 @@ int checkZonesForData(
 	int numRasters,
 	const char * listOfRasterPaths [],
 	const char * zoneRasterPath,
-	//const char * resultDir,
 	const char * tmpDir,
 	float selectionThreshold
 	)
 {
 	runParamsT runParams;
-	//runParams.resultDir = string(resultDir) + "\\";
 	runParams.tmpDir = string(tmpDir) + "\\";
+
 	raster zoneRaster(zoneRasterPath, raster::INPUT);
 	
 	for (int idx = 0; idx < numRasters; idx++)
-	{
+	{	
 		raster croplandRaster(listOfRasterPaths[idx], raster::INPUT);
 		string tmpName = string(listOfRasterPaths[idx]) + "_is_data";
 		raster isDataRaster(tmpName, raster::OUTPUT);
