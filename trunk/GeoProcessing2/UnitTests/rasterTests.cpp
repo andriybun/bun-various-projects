fbb4714093942f00eed640c654dd45a00ffdf000
andr.bun@gmail.com <andr.bun@gmail.com@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2014-10-12 21:48:05 +0000

    Fixed input files for unit tests, tests themselves;

--- trunk/GeoProcessing2/UnitTests/rasterTests.cpp
+++ trunk/GeoProcessing2/UnitTests/rasterTests.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include <string>
 #include "raster.h"
 #include "SpatialAnalyst.h"
 
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace UnitTests
 {		
@@ -15,8 +16,10 @@ namespace UnitTests
 		
 		TEST_METHOD(TestGetCellArea)
 		{
-			raster inRaster("incrementHdr", raster::INPUT);
-			Assert::AreEqual(inRaster.getCellValue(1, 1), (float)1);
+			string home("D:\\Workspace\\IIASA\\GeoProcessing2\\_testData\\");
+			raster inRaster(home + "increment", raster::INPUT);
+			Assert::AreEqual((float)1, inRaster.getCellValue(1, 4));
+			Assert::AreEqual((float)16, inRaster.getCellValue(4, 1));
 		}
 
 	};
