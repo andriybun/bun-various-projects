03d2dd1f1da97c9e2a72e7cd8d7f9abd069fc374
andr.bun@gmail.com <andr.bun@gmail.com@8e31e031-ea9d-cf67-1966-e0f2b5892f1d>
2014-11-17 14:30:09 +0000

    Fixed bug with conditional clip tool

--- trunk/Cropland Tools/Common/mainConClip.py
+++ trunk/Cropland Tools/Common/mainConClip.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     inConditionalLayer = sys.argv[1]
 #    inValueLayer       = sys.argv[2]
     inValueLayerVector = (sys.argv[2].replace("'","")).split(";")
-    numRasters         = len(inValueLayerVector)
+    
+    numRasters         = len(inValueLayerVector)    
     
     condition          = sys.argv[3]
     outLayer           = sys.argv[4]
@@ -18,9 +19,10 @@ if __name__ == "__main__":
     if numRasters > 1:
         resultDir = os.path.dirname(outLayer)
         for raster in inValueLayerVector:
-            outLayer = resultDir + "\\cl_" + os.path.basename(raster)
-            ConClip(inConditionalLayer, raster, condition, outLayer)
+            outLayerVector.append(resultDir + "\\cl_" + os.path.basename(raster))
+        ConClip(inConditionalLayer, inValueLayerVector, condition, outLayerVector)
     else:
-        ConClip(inConditionalLayer, inValueLayerVector[0], condition, outLayer)
+        outLayerVector.append(outLayer)
+        ConClip(inConditionalLayer, inValueLayerVector, condition, outLayerVector)
 
     
